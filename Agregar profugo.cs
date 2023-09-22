c83303468670d454bc78f836ae832617ab5184c4
root <mgiraudo@ies21.edu.ar>
2023-09-22 12:59:31 -0300

    ofg

---
+++ Agregar profugo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Justicia
+{
+    public partial class Agregar_profugo : Form
+    {
+        public Agregar_profugo()
+        {
+            InitializeComponent();
+        }
+    }
+}
